150f586725b51d3b71a73290cda70e3c1ecf2634
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-20 11:29:27 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class LintCommand(Command):
 
 setup(name="osbuild",
       packages=["osbuild", "osbuild.plugins", "osbuild"],
-      version="0.12",
+      version="0.13",
       description="Pull, build and test multiple source modules",
       author="Daniel Narvaez",
       author_email="dwnarvaez@gmail.com",
