ad25c7c42b31c7b6128cdb6cfdc932fcd565bf35
Daniel Narvaez <dwnarvaez@gmail.com>
2014-05-06 00:13:07 +0200

    Missing imports

--- osbuild/command.py
+++ osbuild/command.py
@@ -13,11 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import logging
 import subprocess
 import time
 
 import plog
+
+from osbuild import config
 
 
 def run(args, retry=0, watch_log=None):
